8d2e3f21ccf9e51a1113b3d555296ee9253f5897
szabohuba <72753930+szabohuba@users.noreply.github.com>
2023-05-28 15:59:16 +0300

    cool

---
+++ process-signup.php
@@ -0,0 +1,59 @@
+<?php
+
+//nev ellenorzese, ne legyen ures
+if (empty($_POST["name"])) {
+    die("Name is required");
+}
+//email ellenorzese
+if ( ! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+    die("Valid email is required");
+}
+//jelszo hosszabb legyen mint 8 karakter
+if (strlen($_POST["password"]) < 8) {
+    die("Password must be at least 8 characters");
+}
+
+if ( ! preg_match("/[a-z]/i", $_POST["password"])) {
+    die("Password must contain at least one letter");
+}
+
+if ( ! preg_match("/[0-9]/", $_POST["password"])) {
+    die("Password must contain at least one number");
+}
+
+if ($_POST["password"] !== $_POST["password_confirmation"]) {
+    die("Passwords must match");
+}
+
+///ezt a fugvenyt azert hasznalom, hogy leyen lekodolva a password, biztonsag erdekeben
+$password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+$mysqli = require __DIR__ . "/database.php";
+
+$sql = "INSERT INTO user (name, email, password_hash)
+        VALUES (?, ?, ?)";
+        
+$stmt = $mysqli->stmt_init();
+
+if ( ! $stmt->prepare($sql)) {
+    die("SQL error: " . $mysqli->error);
+}
+
+$stmt->bind_param("sss",
+                  $_POST["name"],
+                  $_POST["email"],
+                  $password_hash);
+                  
+if ($stmt->execute()) {
+
+    header("Location: signup-success.html");
+    exit;
+    
+} else {
+    
+    if ($mysqli->errno === 1062) {
+        die("email already taken");
+    } else {
+        die($mysqli->error . " " . $mysqli->errno);
+    }
+}
